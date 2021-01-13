ebea56b4e23a4a330b8d767515ee006b7aba62f9
Joonas <asd@asd.fi>
2021-01-13 19:03:43 +0200

    task3 done

--- src/index.js
+++ src/index.js
@@ -1,97 +1,86 @@
-console.log('Hello console!');
+const coursesEn = [
+  'Hamburger, cream sauce and poiled potates',
+  'Goan style fish curry and whole grain rice',
+  'Vegan Chili sin carne and whole grain rice',
+  'Broccoli puree soup, side salad with two napas',
+  'Lunch baguette with BBQ-turkey filling',
+  'Cheese / Chicken / Vege / Halloum burger and french fries'];
 
-const numberInput = document.querySelector('#number-input');
-const numberButton = document.querySelector('#number-button');
-const instructions = document.querySelector('#instructions');
-const previousGuesses = document.querySelector('#previous-guesses');
-const guessResult = document.querySelector('#guess-result');
-const resetButton = document.querySelector('#reset-button');
-const statistics = document.querySelector('#statistics');
-const guessArray = [];
+const coursesFi = [
+  'Jauhelihapihvi, ruskeaa kermakastiketta ja keitettyä perunaa',
+  'Goalaista kalacurrya ja täysjyväriisiä',
+  'vegaani Chili sin carne ja täysjyväriisi',
+  'Parsakeittoa,lisäkesalaatti kahdella napaksella',
+  'Lunch baguette with BBQ-turkey filling',
+  'Juusto / Kana / Kasvis / Halloumi burgeri ja ranskalaiset'];
 
-const maxNumber = 150;
-const minNumber = 25;
-const maxGuesses = 10;
-let guesses = 0;
-const startTime = Date.now();
+const languageButton1 = document.querySelector('#language-button1');
+const sortButton = document.querySelector('#sort-button1');
+const randomButton = document.querySelector('#randomize-button1');
+const menu1 = document.querySelector('#grid-menu1');
+const randomMeal = document.querySelector('#random-meal1');
+let languageFi = true;
+let sortedAsc = false;
+menu1.innerHTML = '';
 
-instructions.innerHTML = `Peli arpoo numeron väliltä ${minNumber}-${maxNumber}, kokeile arvata se enintään ${maxGuesses} arvauksella.`;
+for (const course of coursesFi) {
+  menu1.innerHTML += `<li>${course}</li>`;
+}
 
-let randomNumber = Math.floor(
-  Math.random() * (maxNumber - minNumber) + minNumber);
-
-const checkGuess = () => {
-  if (Number(numberInput.value) < randomNumber && guesses < maxGuesses) {
-    guessResult.style.display = 'block';
-    guessResult.innerHTML = 'Arvauksesi oli liian pieni!';
-    guessArray.push(Number(numberInput.value));
-    previousGuesses.innerHTML = 'Edellisiä arvauksia: ';
-    for (let i = 0; i < guessArray.length; i++) {
-      if (i !== guessArray.length - 1) {
-        previousGuesses.innerHTML += `${guessArray[i]}, `;
-      } else {
-        previousGuesses.innerHTML += `${guessArray[i]}.`;
-      }
-    }
-    guesses++;
-  } else if (Number(numberInput.value) > randomNumber && guesses < maxGuesses) {
-    guessResult.innerHTML = 'Arvauksesi oli liian suuri!';
-    guessResult.style.display = 'block';
-    previousGuesses.innerHTML = 'Edellisiä arvauksia: ';
-    guessArray.push(Number(numberInput.value));
-    for (let i = 0; i < guessArray.length; i++) {
-      if (i !== guessArray.length - 1) {
-        previousGuesses.innerHTML += `${guessArray[i]}, `;
-      } else {
-        previousGuesses.innerHTML += `${guessArray[i]}.`;
-      }
-    }
-    guesses++;
-  } else if (isNaN(numberInput.value)) {
-    guessResult.innerHTML = 'Arvauksesi ei ollut numero...';
-    guessResult.style.display = 'block';
-    previousGuesses.innerHTML = 'Edellisiä arvauksia: ';
-    guessArray.push(Number(numberInput.value));
-    for (let i = 0; i < guessArray.length; i++) {
-      if (i !== guessArray.length - 1) {
-        previousGuesses.innerHTML += `${guessArray[i]}, `;
-      } else {
-        previousGuesses.innerHTML += `${guessArray[i]}.`;
-      }
-    }
-    guesses++;
-  } else {
-    const endTime = Date.now();
-    const timeElapsed = Math.floor((endTime - startTime) / 1000);
-    if (guesses < maxGuesses) {
-      guessResult.innerHTML = 'Voitit!';
-      guessResult.style.backgroundColor = 'lightgreen';
-      guessResult.style.display = 'block';
-      numberButton.disabled = true;
-      numberInput.disabled = true;
-      statistics.innerHTML = `Aikaa kului ${timeElapsed} sekuntia. Käytit ${guesses +
-      1} arvausta`;
-      statistics.style.display = 'block';
-    } else {
-      guessResult.innerHTML = 'Liikaa arvauksia, hävisit!';
-      guessResult.style.backgroundColor = 'red';
-      guessResult.style.color = 'white';
-      guessResult.style.display = 'block';
-      numberButton.disabled = true;
-      numberInput.disabled = true;
-      statistics.innerHTML = `Aikaa kului ${timeElapsed} sekuntia.`;
-      statistics.style.display = 'block';
-    }
-  }
-};
-
-const resetGame = () => {
-  location.reload();
-};
-
-numberButton.addEventListener('click', (evt) => {
+languageButton1.addEventListener('click', (evt) => {
   evt.preventDefault();
-  checkGuess();
+  randomMeal.innerHTML = '';
+  menu1.innerHTML = '';
+  if (languageFi) {
+    for (const course of coursesEn) {
+      menu1.innerHTML += `<li>${course}</li>`;
+    }
+    languageFi = false;
+  } else {
+    for (const course of coursesFi) {
+      menu1.innerHTML += `<li>${course}</li>`;
+    }
+    languageFi = true;
+  }
 });
 
-resetButton.addEventListener('click', resetGame);
+function sortMenu(array, order) {
+  if (!order) {
+    sortedAsc = true;
+    return array.sort();
+  } else {
+    sortedAsc = false;
+    return array.sort().reverse();
+  }
+}
+
+sortButton.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  randomMeal.innerHTML = '';
+  if (languageFi) {
+    menu1.innerHTML = '';
+    for (const course of sortMenu(coursesFi, sortedAsc)) {
+      menu1.innerHTML += `<li>${course}</li>`;
+    }
+  } else {
+    menu1.innerHTML = '';
+    for (const course of sortMenu(coursesEn, sortedAsc)) {
+      menu1.innerHTML += `<li>${course}</li>`;
+    }
+  }
+});
+
+function randomizeMeal() {
+  if(languageFi){
+    const random = Math.floor(Math.random() * coursesFi.length);
+    randomMeal.innerHTML = `Kokeile tätä: ${coursesFi[random]}`;
+  } else {
+    const random = Math.floor(Math.random() * coursesEn.length);
+    randomMeal.innerHTML = `Try this: ${coursesEn[random]}`;
+  }
+}
+
+randomButton.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  randomizeMeal();
+});
