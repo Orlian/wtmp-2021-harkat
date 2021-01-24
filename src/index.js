07794da16fc5a069060299e0aff00ff2c9150c10
Joonas <asd@asd.fi>
2021-01-24 18:20:58 +0200

    week2 done

--- src/index.js
+++ src/index.js
@@ -6,14 +6,23 @@ const instructions = document.querySelector('#instructions');
 const previousGuesses = document.querySelector('#previous-guesses');
 const guessResult = document.querySelector('#guess-result');
 const resetButton = document.querySelector('#reset-button');
+const playButton = document.querySelector('#algorithm-button');
 const statistics = document.querySelector('#statistics');
-const guessArray = [];
+let guessArray = [];
 
-const maxNumber = 150;
-const minNumber = 25;
+const maxNumber = 100;
+const minNumber = 1;
+const algorithmArray = [];
 const maxGuesses = 10;
 let guesses = 0;
+const repeats = 2000;
 const startTime = Date.now();
+
+playButton.innerHTML = `Juoksuta ${repeats} kertaa!`;
+
+for (let i = minNumber; i <= maxNumber; i++) {
+  algorithmArray.push(i);
+}
 
 instructions.innerHTML = `Peli arpoo numeron väliltä ${minNumber}-${maxNumber}, kokeile arvata se enintään ${maxGuesses} arvauksella.`;
 
@@ -101,7 +110,75 @@ resetButton.addEventListener('click', resetGame);
 // riippuen oliko arvaus liian suuri vai pieni.
 
 // Einstein-algoritmi //
+/**
+ *
+ * @param {*[]} array
+ * @returns {number}
+ */
 
-const playGame = (array, jump, result) => {
-  const maxIndex = array.length - 1;
+const playGame = (array) => {
+  /** Arvotaan algoritmille satunnainen etsittävä numero
+   * ennalta määritetyltä väliltä **/
+  let randomNumber = Math.floor(
+    Math.random() * (maxNumber - minNumber) + minNumber);
+  /** Määritellään aloitus ja lopetus indeksit jonolle **/
+  let startIndex = 0;
+  let maxIndex = array.length - 1;
+  guessArray = [];
+  guesses = 0;
+
+  while (startIndex <= maxIndex) {
+    /** Lasketaan jonon keskipiste **/
+    const middlepoint = startIndex + Math.floor((maxIndex - startIndex) / 2);
+
+    /** Palautetaan vastaus jos se on oikea **/
+    if (randomNumber === array[middlepoint]) {
+      guessArray.push(array[middlepoint]);
+      guesses++;
+      return array[middlepoint];
+    }
+
+    /** Valitaan kumpaan suuntaan lähdetään riippuen arvauksesta **/
+    if (array[middlepoint] < randomNumber) {
+      startIndex = middlepoint + 1;
+      guesses++;
+      guessArray.push(array[middlepoint]);
+    } else {
+      maxIndex = middlepoint - 1;
+      guesses++;
+      guessArray.push(array[middlepoint]);
+    }
+  }
+
+  /** Jos mitään ei löydy jostain syystä **/
+  return -1;
 };
+
+const algorithmStats = (repeats) => {
+  let avgGuesses = 0;
+  let maxGuesses = 1;
+  let minGuesses = 1;
+
+  for (let i = 1; i <= repeats; i++) {
+    playGame(algorithmArray);
+    avgGuesses += guesses;
+    if (maxGuesses < guesses) {
+      maxGuesses = guesses;
+    }
+    if (minGuesses > guesses) {
+      minGuesses = guesses;
+    }
+  }
+  avgGuesses = (avgGuesses / repeats).toFixed(2);
+  console.log('Repeats:', repeats, '\nAverage guess number:', avgGuesses,
+    '\nMaximum guess number in a round:', maxGuesses,
+    '\nMinimum guess number in a round:', minGuesses);
+};
+
+/** 2000 toistolla arvauslukumäärän keskiarvo leijailee melko tarkasti 5,8 paikkeilla
+ * Maksimi arvaus käytännössä ja teoriassa on 7 ja minimi 1 **/
+
+playButton.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  algorithmStats(repeats);
+});
